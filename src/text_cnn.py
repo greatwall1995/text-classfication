dd40cbb58bbf8cd83f3399b6040ca286aaf9aa4c
greatwall1995 <greatwall1995@126.com>
2017-03-11 19:52:10 +0800

    fix bug

--- src/text_cnn.py
+++ src/text_cnn.py
@@ -5,8 +5,7 @@ import json
 import data_helpers
 import logging
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 def get_voc(name):
 	dic = data_helpers.get_dict(name)
@@ -38,26 +37,24 @@ def load_data(name, voc, input_size=2048, mode='char'):
 			content.append(translate(obj['content'], voc, input_size, mode = 'char'))
 			#print content[len(content) - 1]
 			if (name != 'test'):
-				label.append(obj['label'])
+				label.append(int(obj['label']))
 			id.append(obj['id'])
 	print ''
 	return [content, label, id]
 
-def make_text_cnn(input_size, voc_size, num_embed, filter_size,
+def make_text_cnn(batch_size, input_size, voc_size, num_embed, filter_size,
 				  num_filter, fc_size, dropout):
 	input_x = mx.sym.Variable('data')
-	input_y = mx.sym.Variable('softmax_label')
 	embed_layer = mx.sym.Embedding(data=input_x, input_dim=voc_size, output_dim=num_embed,
 								  name='embed')
-	cnn = mx.sym.Reshape(data=embed_layer,
-								 target_shape=(batch_size, 1, input_size, num_embded))
-	t = num_embded
+	cnn = mx.sym.Reshape(data=embed_layer, shape=(batch_size, 1, input_size, num_embed))
+	t = num_embed
 	for i in xrange(len(filter_size)):
 		fs = filter_size[i]
 		nf = num_filter[i]
 		cnn = mx.sym.Convolution(name='conv%d' % i, data=cnn, kernel=(fs, t), num_filter=nf)
 		cnn = mx.sym.Activation(data=cnn, act_type='relu')
-		cnn = mx.sym.BatchNorm(data=cnn)
+		#cnn = mx.sym.BatchNorm(data=cnn)
 		cnn = mx.sym.Pooling(data=cnn, pool_type='max', kernel=(fs, 1), stride=(1,1))
 		if dropout > 0.0:
 			cnn = mx.sym.Dropout(data=cnn, p=dropout)
@@ -65,20 +62,62 @@ def make_text_cnn(input_size, voc_size, num_embed, filter_size,
 	fc = cnn
 	for i in xrange(len(fc_size)):
 		fc = mx.sym.FullyConnected(name='fc%d' % i, data=fc, num_hidden=fc_size[i])
-	sm = mx.sym.SoftmaxOutput(name='softmax', data=fc, label=input_y)
+  		fc = mx.sym.Activation(data=fc, act_type='relu')
+    	#fc = mx.sym.BatchNorm(data=fc)
+	fc = mx.sym.FullyConnected(name='final', data=fc, num_hidden=1)
+	sm = mx.sym.LogisticRegressionOutput(data=fc)
 	return sm
 
 def setup_cnn_model(ctx, batch_size, input_size, voc_size, num_embed, filter_size,
 					num_filter, fc_size, dropout=0.0):
 
-	return make_text_cnn(input_size, voc_size, num_embed, filter_size,
+	return make_text_cnn(batch_size, input_size, voc_size, num_embed, filter_size,
 						num_filter, fc_size, dropout)
 
 
-def train_cnn(cnn_model, data_train, data_val, data_test, batch_size):
+def calc_auc(label, pred):
+	s = zip(pred, label)
+	s.sort(reverse = True)
+	#print pred
+	#print label
+	n0 = 0
+	n1 = 0
+	for l in label:
+		if l == 0:
+			n0 += 1
+		else:
+			n1 += 1
+	res = 0.0
+	ct0 = 0
+	ct1 = 0
+	for (p, l) in s:
+		if l == 1:
+			ct1 += 1
+		else:
+			ct0 += 1
+			res += (ct1 / (n1 + 0.0)) * (1. / (n0 + 0.0))
+	return res
+
+class eval_call(object):
+	def __call__(self, param):
+		print "eval_call()"
+		print param
+		print ""
+
+def epoch_call():
+	def _callback(iter_no, sym, arg, aux):
+		print "epoch_call()"
+		print "iter_no = ", iter_no
+		print "sym = ", sym
+		print "arg = ", arg
+		print "aux = ", aux
+		print ""
+	return _callback
+
+def train_cnn(ctx, cnn_model, data_train, data_val, data_test, batch_size):
 	print 'Trainning model...'	
-	epoch = 20
-	learning_rate = 1e-3
+	epoch = 5
+	learning_rate = 1e-1
 	reg = 5e-4
 	
 	model = mx.model.FeedForward(
@@ -86,28 +125,49 @@ def train_cnn(cnn_model, data_train, data_val, data_test, batch_size):
 		ctx=ctx,
 		optimizer='adam',
 		learning_rate=learning_rate,
-		wd=REG,
+		numpy_batch_size=batch_size,
+		wd=reg,
 		num_epoch=1
 	)
 	
-	train = mx.io.NDArrayIter(data_train[0], data_train[1], batch_size=batch_size, shuffle=True)
+	print np.array(data_train[0]).shape
+	print np.array(data_train[1]).shape
+	print np.array(data_val[0]).shape
+	print np.array(data_val[1]).shape
+	
+	print batch_size
+	
+	train = mx.io.NDArrayIter(np.array(data_train[0]), np.array(data_train[1]),
+							  batch_size=batch_size, label_name='logisticregressionoutput0_label')#, shuffle=True)
+	print batch_size
+	val = mx.io.NDArrayIter(np.array(data_val[0]), np.array(data_val[1]),
+							batch_size=batch_size)
+	print batch_size
+	test = mx.io.NDArrayIter(np.array(data_test[0]), np.array(data_test[1]),
+							 batch_size=batch_size)
 	
 	best_model = None
 	best_auc = -1
 	
-	for i in xrange(20):
-		model.fit(X=train, batch_end_callback = mx.callback.Speedometer(batch_size, 50))
-		prob = model.predict(data_val[0])
+	#model.fit(X=train, eval_data=val,
+	#		  batch_end_callback = mx.callback.Speedometer(batch_size, 1),
+	#		  epoch_end_callback = epoch_call(),
+	#		  eval_end_callback = eval_call())
+	
+	for i in xrange(epoch):
+		model.fit(X=train, batch_end_callback = mx.callback.Speedometer(batch_size, 1))
+		prob = model.predict(np.array(data_val[0]))#, num_batch=batch_size)
+		print prob
 		auc = calc_auc(data_val[1], prob)
-		model.save('../param/model', i)
+		print "Iter %d, AUC = %f" % (i, auc)
+		model.save('model', i)
 		if auc > best_auc:
 			best_auc = auc
 			best_model = model
-		print "Iter %d, AUC = %f" % (i, auc)
 	
 	print "Best AUC = %f" % best_auc
-	best_model.save('../param/best_model', 0)
-	prob = best_model.predict(data_text[0])
+	best_model.save('best_model', 0)
+	prob = best_model.predict(test, num_batch=batch_size)
 	f_out1 = file('../data/test.txt', 'w')
 	print prob
 	f_out1.close()
@@ -121,11 +181,12 @@ def main():
 	
 	mode = 'char'	# mode = 'char' or 'word'
 	input_size = 2048
-	num_embed = 256
+	num_embed = 32#128
 	batch_size = 64
 	filter_size = [5, 3]
-	num_filter = [256, 512]
-	fc_size = [200]
+	num_filter = [16, 18]#[128, 256]
+	fc_size = [50]#[200]
+	dropout = 0.5
 	
 	voc = get_voc('dict1')
 	
@@ -141,13 +202,18 @@ def main():
 	data_val = load_data('val', voc, mode=mode)
 	data_test = load_data('test', voc, mode=mode)
 	
+	#print data_train[0]
+	#print '------------------'
+	#print data_val[0]
+	#print '------------------'
+	
 	print 'train shape', len(data_train[0])
 	print 'val shape', len(data_val[0])
 	print 'test shape', len(data_test[0])
 	
 	cnn_model = setup_cnn_model(mx.cpu(0), batch_size, input_size, voc_size, num_embed,
-								filter_size, num_filter, fc_size, dropout = 0.5)
-	train_cnn(cnn_model, data_train, data_val, data_test, batch_size)
+								filter_size, num_filter, fc_size, dropout)
+	train_cnn(mx.cpu(0), cnn_model, data_train, data_val, data_test, batch_size)
 
 if __name__ == '__main__':
 	main()
